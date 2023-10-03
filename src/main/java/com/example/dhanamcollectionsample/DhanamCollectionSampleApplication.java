f213dbac6d86e57b9b00c3ff656afc0e3a1900f8
Lalu-Mahato <135944020+Lalu-Mahato@users.noreply.github.com>
2023-10-03 17:47:04 +0530

    Project setup with basic requirements

---
+++ src/main/java/com/example/dhanamcollectionsample/DhanamCollectionSampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.dhanamcollectionsample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DhanamCollectionSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DhanamCollectionSampleApplication.class, args);
+	}
+
+}
